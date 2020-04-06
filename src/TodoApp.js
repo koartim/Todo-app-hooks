dd738e807d41ce9a07d1f50ad6727acc5cf06947
Tim Koar <koartimothy@gmail.com>
2020-04-06 13:04:08 -0400

    added material ui toolbar

---
+++ src/TodoApp.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+
+const TodoApp = () => {
+    return (
+        <Paper style={{
+            padding: 0,
+            margin: 0,
+            height: "100vh",
+            backgroundColor: "#fafafa"
+        }}
+        elevation={0}
+        >
+            <AppBar color='primary' position='static' style={{height: "64px"}}>
+                <ToolBar>
+                    <Typography color='inherit'>Todos With Hooks</Typography>
+                </ToolBar>
+            </AppBar>
+        </Paper>
+    )
+}
+export default TodoApp;
